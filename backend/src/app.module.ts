6cc898562a867f5889c70c52c17d4db1622fbc11
EvangeRodriguez <eva.irodriguez93@gmail.com>
2023-08-21 12:56:05 +0200

    funcionalidades backend

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { GameModule } from './game/game.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://memorygame09:<UAp5BsaipgmC4z9V>@memory-game.odjtalb.mongodb.net/?retryWrites=true&w=majority',
+    ),
+    GameModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
