afe0a9efed82812bd9e71a8b765079d27323d7ac
EvangeRodriguez <eva.irodriguez93@gmail.com>
2023-08-22 10:17:09 +0200

    day 1 finished

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,18 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vitest/config'
+import react from '@vitejs/plugin-react'
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/test/setup.js',
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
-});
+  test:{
+    globals:true,
+    environment: 'jsdom',
+    setupFiles:  ['src/tests/setup.ts'],
+  }
+})
