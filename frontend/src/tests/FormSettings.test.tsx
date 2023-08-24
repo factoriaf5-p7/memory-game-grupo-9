82464769c4b4f35b00f605a12b2caa6dadec07bb
Acrata <hxtr227@gmail.com>
2023-08-24 19:29:35 +0200

    Accept changes in settings

--- frontend/src/tests/FormSettings.test.tsx
+++ frontend/src/tests/FormSettings.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, fireEvent, jest } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import FormSettings from '../components/FormSettings';
 
 const options = ['group1', 'group2']; // Replace with actual options
