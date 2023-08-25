f417d721e2ad0eacf83311be92cb1c6321e4fd7c
Acrata <hxtr227@gmail.com>
2023-08-25 18:47:58 +0200

    working on card match

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -1,5 +1,5 @@
 
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
 
 interface CardProps {
   name: string;
@@ -8,14 +8,26 @@ interface CardProps {
 
 export function Card({ name, img }: CardProps) {
   const [isFlipped, setIsFlipped] = useState(false);
+  const [onGame, setOnGame] = useState([])
+  let currentCard = useRef(null)
 
   const handleCardClick = (e) => {
+    // const elemt = currentCard.current.dataset.image.toLowerCase().split('')[0]
     setIsFlipped(!isFlipped);
-    console.table(e.target)
+    setOnGame([...onGame,currentCard])
+    console.log(currentCard)
+    // console.log(currentCard.current.dataset.image.toLowerCase().split(' '))
+    // console.log(onGame)
   };
+  
+  useEffect(() => {
+    // setOnGame([...onGame, currentCard.current])
+    console.log(onGame)
+    // onGame.map(card => card.classList.add('red'))
+  }, [isFlipped])
 
   return (
-    <div className={`card-box h-28 aspect-square ${isFlipped ? 'flipped' : ''} `} onClick={handleCardClick} data-testid="card" data-image={name}>
+    <div ref={el => currentCard = el} className={`card-box h-28 aspect-square ${isFlipped ? 'flipped' : ''} `} onClick={handleCardClick} data-testid="card" data-image={name}>
        <div className={`card w-full h-full transition-transform duration-500 transform ${isFlipped ? 'rotate-y-180' : ''} bg-[url('carta.jpg')]`}>
        <div className={`side front p-2 rounded-md shadow-md ${isFlipped ? 'hidden' : ''}`} data-testid="front-side">
         </div>
